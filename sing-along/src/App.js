4e3b15a122dc281b23132c9ec21eb1b110fb48f6
George Koutsaris <georgekoutsaris@hotmail.com>
2024-09-25 22:47:18 -0400

    #1 #2 worked on the backbone and styling of home page

--- sing-along/src/App.js
+++ sing-along/src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home-page/home-page';
 
 function App() {
-  
-  document.title = 'Home: Sing-Along Subtitle Generator'
   
   return (
     <div>
--- sing-along/src/pages/home-page/home-page.js
+++ sing-along/src/pages/home-page/home-page.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import './home-page.styles.css'
+import dsasLogo from '../../assets/images/logos/dsas_logo.webp'
 
 function HomePage() {
+    document.title = 'Home: Sing-Along Subtitle Generator'
     const navigate = useNavigate();
 
     return (
-        <div>
-            <button 
-                onClick={()=>navigate('/')}>Start Creating Your Subtitles!
+        <div className='main'>
+            <div className='home-page-content'>
+                <img className='dsas-logo' src={dsasLogo} alt='dsas-logo'/> 
+                <h1 className='home-page-subtitle'>SUBTITLE GENERATOR</h1>
+                <h2 className='home-page-subtitle2'>So you at home can sing along!</h2>
+            </div>
+            <button className='start-button'
+                onClick={()=>navigate('/')}>Get Started!
             </button>
         </div>
     )
