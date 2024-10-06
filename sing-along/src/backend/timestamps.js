fb800c581ae24c32dc4136a9ecbcf3db2c3ca0d7
George Koutsaris <georgekoutsaris@hotmail.com>
2024-10-06 18:53:20 -0400

    #12 debugged timestamp helper methods

--- sing-along/src/backend/timestamps.js
+++ sing-along/src/backend/timestamps.js
@@ -1,6 +1,5 @@
 export const to_milliseconds = (timestamp) =>{ // ex. "0:00:24.816" (quotes included)
-    let stamp = timestamp.substring(1, timestamp.length-1);
-    let hms = stamp.split('.')[0].split(':')
+    let hms = timestamp.split('.')[0].split(':')
     let hr = hms[0]
     let min = hms[1]
     let sec = hms[2]
@@ -8,7 +7,7 @@ export const to_milliseconds = (timestamp) =>{ // ex. "0:00:24.816" (quotes incl
     let hour_in_ms = parseInt(hr) * 60 * 60 * 1000
     let min_in_ms = parseInt(min) * 60 * 1000
     let sec_in_ms = parseInt(sec) * 1000
-    let ms = parseInt(stamp.split('.')[1])
+    let ms = parseInt(timestamp.split('.')[1])
 
     return (hour_in_ms + min_in_ms + sec_in_ms + ms)
 }  
@@ -18,27 +17,24 @@ export const time_difference = (stamp1, stamp2) => {
 }
     
 export const millis_to_stamp = (total_ms) => {
-    const hrs = total_ms // 3600000
-    const mins = (total_ms % 3600000) // 60000
-    const secs = (total_ms % 60000) // 1000
-    const ms = total_ms % 1000;
-
-    const _hrs = (hrs > 9) ? toString(hrs) : `0${hrs}`
-    const _mins = (mins > 9) ? toString(mins) : `0${mins}`
-    const _secs = (secs > 9) ? toString(secs) : `0${secs}`
+    const hrs = parseInt(total_ms / 3600000)
+    const mins = parseInt((total_ms % 3600000) / 60000)
+    const secs = parseInt((total_ms % 60000) / 1000)
+    const ms = parseInt(total_ms % 1000);
+  
+    const _hrs = (hrs > 9) ? hrs : `0${hrs}`
+    const _mins = (mins > 9) ? mins : `0${mins}`
+    const _secs = (secs > 9) ? secs : `0${secs}`
     
-    let _ms = ""
+    let _ms = ms
     if (ms <= 9) {
         _ms = `00${ms}`
     }
     else if (ms >= 10 && ms <= 99) {
         _ms = `0${ms}`
     }
-    else {
-        _ms = toString(ms)
-    }
 
-    return `\"${_hrs}:${_mins}:${_secs}.${_ms}\"`
+    return `"${_hrs}:${_mins}:${_secs}.${_ms}"`
 }
 
 export const alter_timestamp = (stamp, millis) => {
