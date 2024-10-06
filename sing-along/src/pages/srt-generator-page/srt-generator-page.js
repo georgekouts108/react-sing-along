281ee551e73f86179622a1db6dcc6026c61b2947
George Koutsaris <georgekoutsaris@hotmail.com>
2024-10-06 17:52:44 -0400

    #12 started panel for .srt file generator options

--- sing-along/src/pages/srt-generator-page/srt-generator-page.js
+++ sing-along/src/pages/srt-generator-page/srt-generator-page.js
@@ -6,7 +6,10 @@ function SrtGeneratorPage() {
     const location = useLocation();
 
     const currentData = location.state?.data;
+    console.log(currentData);
     const [fileContent, setFileContent] = useState("");
+
+    const [willCreateNewFile, setWillCreateNewFile] = useState(true);
 
     // This function handles the file input change event
     const handleFileChange = (e) => {
@@ -23,14 +26,49 @@ function SrtGeneratorPage() {
 
     return (
         <div>
-        <h3>Upload an .srt File</h3>
-        <input type="file" accept=".srt" onChange={handleFileChange} />
-        {fileContent && (
-            <div>
-            <h4>File Content:</h4>
-            <pre>{fileContent}</pre>
-            </div>
-        )}
+            <h1>Select how you want to generate your subtitle file.</h1>
+
+            <table style={{border: '3px solid black', textAlign: 'center'}}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input defaultChecked={true} id='srt-gen-new' onClick={()=>setWillCreateNewFile(true)} type='radio' name='srt-gen-method'/>
+                            <label htmlFor='srt-gen-new'>Create new .srt file from scratch</label>
+                        </td>
+                        <td>
+                            <input id='srt-gen-add' onClick={()=>setWillCreateNewFile(false)} type='radio' name='srt-gen-method'/>
+                            <label htmlFor='srt-gen-add'>Add lyrics to existing .srt file</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2} style={{border: '3px solid black', textAlign: 'center'}}>
+                            { 
+                                willCreateNewFile && 
+                                <div>
+                                    <button>Generate Subtitle File</button>
+                                </div>
+                            }
+                            {/* { 
+                                !willCreateNewFile && 
+                                <div>
+                                    <h3>Upload an .srt file</h3>
+                                    <input type="file" accept=".srt" onChange={handleFileChange} />
+                                    {
+                                    fileContent && (
+                                        <div>
+                                            <h4>File Content:</h4>
+                                            <pre>{fileContent}</pre>
+                                        </div>
+                                    )}
+                                </div>
+                            } */}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        
+        
+        
         </div>
     );
 }
