acaf08f19a8b7f782c801d7dd34810ae6e863284
George Koutsaris <georgekoutsaris@hotmail.com>
2024-10-25 03:09:53 -0400

    #18 implemented useful assets

--- sing-along/src/pages/combine-srts-page/combine-srts-page.js
+++ sing-along/src/pages/combine-srts-page/combine-srts-page.js
@@ -6,12 +6,17 @@ function CombineSRTPage() {
 
     const [loadedFileContent, setLoadedFileContent] = useState('')
     const [fileContents, setFileContents] = useState([]);
+    
+    const [loadedFileName, setLoadedFileName] = useState('')
+    const [fileNames, setFileNames] = useState([]);
 
     const handleFileChange = (e) => {    
         const file = e.target.files[0];
+        
         const reader = new FileReader();
         reader.onload = (event) => {
             setLoadedFileContent(event.target.result);
+            setLoadedFileName(file.name);
         };
         reader.readAsText(file);
     };
@@ -19,13 +24,20 @@ function CombineSRTPage() {
     const addSRTContent = () => {
         const _fc = fileContents;
         _fc.push(loadedFileContent);
+
+        const _fn = fileNames;
+        _fn.push(loadedFileName);
+
+        setFileNames(_fn);
         setFileContents(_fc);
+
         clearLoadedSRT();
     };
 
     const clearLoadedSRT=()=>{
         document.getElementById('fileInput').value = '';
         setLoadedFileContent('');
+        setLoadedFileName('')
     };
 
     const downloadCombinedSRT = () => {
@@ -35,12 +47,41 @@ function CombineSRTPage() {
         }
         downloadSRT(combinedSRT);
     }
-
+    const deleteSRT = (index) => {
+        const updatedFnames = []
+        const updatedFcontents = []
+        for (let i = 0; i < fileNames.length; i++) {
+            if (i !== index) {
+                updatedFnames.push(fileNames[i])
+                updatedFcontents.push(fileContents[i])
+            }
+        }
+        setFileNames(updatedFnames);
+        setFileContents(updatedFcontents);
+    }
     return (
         <div style={{textAlign:'center'}}>
             <h1>Combine Multiple Subtitle Files</h1>
             <hr/><hr/><br/>
-            <h3>Number of subtitle (.srt) files loaded: {fileContents.length}</h3>
+            <h3>The lyrics in the following subtitle (.srt) files will be added in order from top to bottom:</h3>
+            {
+                fileContents.length === 0 && <h3 style={{color:'red'}}>No files added</h3>
+            }
+            {
+                fileContents.length > 0 &&
+                
+                    <div>
+                        {
+                            [...Array(fileNames.length).keys()].map((index) => (
+                                <h4 key={index}>
+                                    <span  style={{color:'blue'}}>{fileNames[index]}</span>&nbsp;&nbsp;&nbsp;
+                                    <button onClick={()=>deleteSRT(index)}>Delete This File</button>
+                                </h4>
+                            ))
+                        }
+                    </div>
+            }
+            <hr/><hr/>
             <input id='fileInput' type="file" accept=".srt" onChange={handleFileChange} />
             <br/><br/>
             <button disabled={!loadedFileContent} onClick={()=>{addSRTContent()}}>Add Loaded SRT Content to Queue</button><br/><br/>
